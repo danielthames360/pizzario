55642acc8791865a328fed4b7f5995d182cfcb24
Daniel Cespedes <danielthames360@gmail.com>
2024-03-05 13:35:40 -0400

    Nuevos cambios del header y poco del footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,126 +5,115 @@ import React from 'react'
 export const Footer = () => {
   return (
     <>
-      <div className='h-[35vh] w-full overflow-hidden xl:h-[45vh]'>
-
-        <div className='relative h-[70%] w-full overflow-hidden'>
-
-        <div className='footer w-full h-full bg-center'></div>
-
-        <div className='overflow-hidden w-4/5 h-full mx-auto flex flex-col justify-around items-center'>
-
-          <div className='w-full h-[70%] flex justify-center items-center'>
-          <Image
-            src={'/images/footer/rioLovers.png'}
-            width={120}
-            height={100}
-            className='sm:w-[180px]
+      <div className='h-[40vh] sm:h-[45vh] w-full overflow-hidden bg-black'>
+        <div className='relative h-[70%] w-full overflow-hidden footer'>
+          <div className='overflow-hidden w-4/5 h-full mx-auto flex flex-col justify-around items-center '>
+            <Image
+              src={'/images/footer/rioLovers.png'}
+              width={1040}
+              height={780}
+              className='
+              mt-5
+                w-[180px]
+                sm:w-[230px]
             md:w-[210px]
             lg:w-[270px]
             xl:w-[300px]'
-            alt='footer'
-          />
-          </div>
-          
-
-          <div className='h-[30%] flex flex-col items-center
-          sm:flex-row'>
-            <Image
-            src={'/images/footer/text.png'}
-            alt='text'
-            width={200}
-            height={200}
-            className='sm:w-[230px]
-            md:w-[250px]
-            lg:w-[280px]
-            xl:w-[290px]'
+              alt='footer'
             />
 
-            <div className='flex gap-2'>
-          <a
-            href='https://www.tiktok.com/@pizzariobo'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='icon'
-          >
-            <Image
-            src={'/images/footer/tiktok.png'} 
-            alt='facebook' 
-            width={20} 
-            height={20}
-            className='xl:w-[30px]'/>
-          </a>
+            <div
+              className='h-[30%] flex flex-col items-center gap-2
+          sm:flex-row'
+            >
+              <p className='text-white leading-6 text-md uppercase font-semibold'>
+                Siguenos en nuestras redes
+              </p>
 
-          <a
-            href='https://www.facebook.com/pizzariosantacruz'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='icon'
-          >
-            <Image
-            src={'/images/footer/facebook.png'} 
-            alt='facebook' 
-            width={20} 
-            height={20}
-            className='xl:w-[30px]'/>
-          </a>
-          <a
-            href='https://www.instagram.com/pizzario.bolivia/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='icon'
-          >
-            <Image
-            src={'/images/footer/instagram.png'} 
-            alt='facebook' 
-            width={22} 
-            height={23}
-            className='xl:w-[33px]'/>
-          </a>
-          
+              <div className='flex gap-2'>
+                <a
+                  href='https://www.tiktok.com/@pizzariobo'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='icon'
+                >
+                  <Image
+                    src={'/images/footer/tiktok.png'}
+                    alt='facebook'
+                    width={30}
+                    height={30}
+                    className='xl:w-[30px]'
+                  />
+                </a>
+
+                <a
+                  href='https://www.facebook.com/pizzariosantacruz'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='icon'
+                >
+                  <Image
+                    src={'/images/footer/facebook.png'}
+                    alt='facebook'
+                    width={30}
+                    height={30}
+                    className='xl:w-[30px]'
+                  />
+                </a>
+                <a
+                  href='https://www.instagram.com/pizzario.bolivia/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='icon'
+                >
+                  <Image
+                    src={'/images/footer/instagram.png'}
+                    alt='facebook'
+                    width={30}
+                    height={30}
+                    className='xl:w-[33px]'
+                  />
+                </a>
+              </div>
             </div>
           </div>
 
-        </div>
-          
-        <Image
-        src={'/images/footer/blackPlant.png'}
-        alt='deco'
-        width={100}
-        height={100}
-        className='absolute -bottom-9 right-0
+          <Image
+            src={'/images/footer/blackPlant.png'}
+            alt='deco'
+            width={100}
+            height={100}
+            className='absolute -bottom-9 right-0
         sm:w-[110px] sm:-bottom-10 sm:right-[5%]
         md:w-[130px] md:-bottom-11
         lg:w-[150px] lg:-bottom-12 lg:right-[10%]
         xl:w-[200px] xl:-bottom-20 xl:right-[12%]'
-        />
+          />
 
-        <Image
-        src={'/images/footer/blackPlants.png'}
-        alt='deco'
-        width={100}
-        height={100}
-        className='absolute -bottom-8 left-0
+          <Image
+            src={'/images/footer/blackPlants.png'}
+            alt='deco'
+            width={100}
+            height={100}
+            className='absolute -bottom-8 left-0
         sm:w-[140px] sm:-bottom-12 sm:left-[5%]
         md:w-[160px] md:left-[6%]
         lg:w-[180px] lg:-bottom-14 lg:left-[15%]
         xl:w-[230px] xl:-bottom-20 xl:left-[12%]'
-        />
-
+          />
         </div>
 
-        <div className='h-[30%] bg-black text-center flex justify-center items-center pt-1 sm:pt-0'>
-        <p className='text-white  leading-5 text-xs sm:text-sm md:text-base'>
-          ©{new Date().getFullYear()} - Pizza Rio
-          <br />
-          <Link href={'/privacy'} className='underline hover:text-gray-300'>
-            Política de privacidad
-          </Link>
-          <br />
-          Todos los derechos reservados
-        </p>
+        <div className='h-[30%] py-5 bg-black text-center flex justify-center items-center sm:pt-0'>
+          <p className='text-white leading-6 text-md'>
+            ©{new Date().getFullYear()} - Pizza Rio
+            <br />
+            <Link href={'/privacy'} className='underline hover:text-gray-300'>
+              Política de privacidad
+            </Link>
+            <br />
+            Todos los derechos reservados
+          </p>
         </div>
-
       </div>
     </>
   )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,23 +3,32 @@ import { PedirYaButton, ScrollDownButton, TextoSucursal } from './'
 
 export const Hero = () => {
   return (
-    <div className='h-screen relative overflow-hidden
-    sm:h-[35vh] lg:h-[30vh] xl:h-[45vh]'>
+    <div
+      className='h-screen relative overflow-hidden
+    sm:h-[35vh] lg:h-[40vh] xl:h-[55vh]'
+    >
+      <div
+        className='hero h-full bg-left 
+      sm:bg-[url(/images/background.png)] sm:bg-center'
+      ></div>
 
-      <div className='hero h-full bg-left 
-      sm:bg-[url(/images/background.png)] sm:bg-center'></div>
-
-      <div className='w-4/5 mx-auto flex flex-col justify-center
-      sm:h-full'>
-
-        <div className='h-[90vh] pt-10 flex flex-col justify-between items-center sm:h-full sm:pt-0 sm:flex-row sm:justify-center
-        md:justify-evenly
-        lg:justify-around'>
-
-          <div className='flex flex-col justify-evenly 
-          sm:justify-stretch items-center
-          xl:flex-row xl:w-[70%] xl:justify-evenly'>
-
+      <div
+        className='w-4/5 lg:w-4/6 mx-auto flex flex-col justify-center
+      sm:h-full'
+      >
+        {/* Contenedor del medio */}
+        <div
+          className='h-[90vh] pt-10 flex flex-col justify-between items-center 
+        sm:h-full sm:pt-0 sm:flex-row sm:justify-center sm:gap-12
+        md:justify-evenly md:gap-0
+        lg:justify-around xl:gap-52'
+        >
+          {/* LOGO */}
+          <div
+            className='flex flex-col justify-evenly 
+          sm:justify-stretch items-center gap-4 sm:gap-0
+          '
+          >
             <Image
               src={'/images/logo.png'}
               width={90}
@@ -31,13 +40,13 @@ export const Hero = () => {
               xl:w-[150px]'
             />
 
-            <div className='flex flex-col'>
+            <div className='flex flex-col gap-4 sm:gap-0'>
               <Image
-              src={'/images/slogan.png'}
-              width={350}
-              height={350}
-              alt='slogan'
-              className='sm:w-[280px] 
+                src={'/images/slogan.png'}
+                width={350}
+                height={350}
+                alt='slogan'
+                className='sm:w-[280px] 
               md:w-[300px] 
               lg:w-[350px] 
               xl:w-[480px]'
@@ -45,96 +54,84 @@ export const Hero = () => {
 
               <PedirYaButton />
             </div>
-            
           </div>
-        
-          <div className='relative'>
-          <Image
-            src={'/images/pizza.png'}
-            width={280}
-            height={280}
-            priority
-            alt='pizza'
-            className='mx-2 
-            sm:w-[190px] 
+
+          {/* Pizza */}
+          <div className='relative sm:flex-1 md:flex-initial'>
+            <Image
+              src={'/images/pizza.png'}
+              width={300}
+              height={300}
+              priority
+              alt='pizza'
+              className='mx-2
+            sm:w-[210px] 
             md:w-[210px] 
             lg:w-[250px] 
-            xl:w-[280px]'
-            // className='animate-spin-slow opacity-90 lg:absolute md:-right-32 xl:right-64 lg:h-[550px] lg:w-[550px] lg:-z-10'
-          />
-          
-          <Image
-          src={'/images/deco.png'}
-          alt='deco'
-          width={50}
-          height={50}
-          className='absolute animationPizza bottom-7 -left-3 deco-animation
+            xl:w-[350px]'
+              // className='animate-spin-slow opacity-90 lg:absolute md:-right-32 xl:right-64 lg:h-[550px] lg:w-[550px] lg:-z-10'
+            />
+
+            <Image
+              src={'/images/deco.png'}
+              alt='deco'
+              width={50}
+              height={50}
+              className='absolute animationPizza bottom-7 -left-3 deco-animation
           sm:w-[40px] sm:h-[60px] sm:bottom-1 sm:-left-3
           md:w-[50px] md:h-[70px] md:-left-4
           lg:w-[60px] lg:h-[80px] lg:-left-5
           xl:w-[70px] xl:h-[90px] xl:-left-6'
-          />
+            />
+          </div>
+        </div>
 
-          <Image
+        <Image
           src={'/images/dino.gif'}
           width={230}
           height={90}
           alt='dino'
-          className='absolute -rotate-[12deg] left-52 bottom-[9rem]
-          sm:-rotate-[3deg] sm:bottom-[2.5rem] sm:left-[10rem]  
-          md:w-[500px] h-[280px] md:bottom-[2rem] md:left-[11rem]
-          lg:w-[350px] lg:h-[280px] lg:bottom-[3rem] lg:left-[13rem]
-          xl:w-[380px] xl:h-[280px] xl:left-[16rem]'
-          />
-          </div>
-          
-        </div>
+          className='absolute z-[-1] -rotate-[25deg] top-1/2 -translate-y-1/2 -right-24 h-[230px]
+          sm:-right-32 sm:top-20 sm:-rotate-[16deg]
+          md:w-[280px] md:-rotate-[6deg] md:top-24
+          lg:w-[350px] lg:h-[280px] lg:top-32
+          xl:w-[380px] xl:h-[280px]'
+        />
 
         <Image
-        src={'/images/redPlantL.png'}
-        width={85}
-        height={85}
-        alt='plant'
-        className='absolute -bottom-16 left-2
+          src={'/images/redPlantL.png'}
+          width={85}
+          height={85}
+          alt='plant'
+          className='absolute -bottom-16 left-2
         sm:w-[70px] sm:-bottom-11 sm:left-1 
         md:left-24
         lg:w-[85px] lg:-bottom-12 lg:left-10
-        xl:w-[120px] xl:-bottom-16 xl:left-[5%]'
+        xl:w-[110px] xl:-bottom-16 xl:left-[5%]'
         />
 
         <Image
-        src={'/images/redPlantL.png'}
-        width={85}
-        height={85}
-        alt='plant'
-        className='hidden
-        xl:block xl:absolute xl:w-[70px] xl:-bottom-[4rem] xl:left-[30%]'
-        />
-
-        <Image
-        src={'/images/redPlantR.png'}
-        width={110}
-        height={110}
-        alt='plant'
-        className='absolute -bottom-12 right-4
+          src={'/images/redPlantR.png'}
+          width={110}
+          height={110}
+          alt='plant'
+          className='absolute -bottom-12 right-4
         sm:w-[60px] sm:-bottom-[3rem] sm:left-[19rem]
-        md:left-[21rem]
-        lg:w-[70px] lg:left-[30rem]
-        xl:left-[60%]'
+        md:left-1/2 md:-translate-x-1/2
+        lg:w-[70px]'
         />
 
         <Image
-        src={'/images/redPlantBig.png'}
-        width={75}
-        height={75}
-        alt='plant'
-        className='hidden 
-        sm:block sm:absolute sm:w-[85px] sm:-right-7 sm:-bottom-6 
+          src={'/images/redPlantBig.png'}
+          width={75}
+          height={75}
+          alt='plant'
+          className='invisible 
+        sm:visible sm:absolute sm:w-[85px] sm:-right-7 sm:-bottom-6 
         md:w-[90px] md:right-8 md:-bottom-7
         lg:w-[105px] lg:right-12 lg:-bottom-8
-        xl:w-[150px] xl:-right-10 xl:-bottom-12'
+        xl:w-[130px] xl:right-20 xl:-bottom-12'
         />
-
       </div>
 
       {/* <ScrollDownButton /> */}
