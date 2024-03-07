c51418d4002a622c673c0c5ef5d0f5f8f7200392
Larissa <larissachg@gmail.com>
2024-03-07 12:13:40 -0400

    modificacion horarios

--- src/sucursales/components/PedirYaCard.tsx
+++ src/sucursales/components/PedirYaCard.tsx
@@ -3,11 +3,11 @@ export const PedirYaCard = ({ id }: { id: string | undefined }) => {
   return (
     <>
       <button
-        className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer'
+        className='bg-[#cc1c19] hover:bg-red-700 text-white font-bold py-2 px-4 rounded-3xl cursor-pointer'
         data-glf-cuid='d1feca7c-ffe6-4a02-ac9d-1eca3fb78dab'
         data-glf-ruid={id}
       >
-        Pedir Ya!
+        ¡Pide ahora!
       </button>
     </>
   )
